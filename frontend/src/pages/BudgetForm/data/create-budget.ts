cb557fcb7c33551baccbfdda6a42390c3751267f
Alekmz <amalexmuniz@gmail.com>
2024-04-25 18:16:35 -0300

    fix: change api url

--- frontend/src/pages/BudgetForm/data/create-budget.ts
+++ frontend/src/pages/BudgetForm/data/create-budget.ts
@@ -4,7 +4,7 @@ import axios from "axios"
 export const useCreateBudget = () => {
   const mutation = useMutation({
     mutationFn: (budget) => {
-      return axios.post('http://44.222.209.215:3000/budget', budget)
+      return axios.post('http://54.91.65.78:3000/budget', budget)
     },
   })
 
