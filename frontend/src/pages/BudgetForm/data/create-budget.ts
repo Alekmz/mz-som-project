4e2b70a4cb7bba8fd0ed67d1caaa6686b4f4c074
alex.muniz <alex.muniz@aquare.la>
2024-05-25 16:36:28 -0300

    fix: new url

--- frontend/src/pages/BudgetForm/data/create-budget.ts
+++ frontend/src/pages/BudgetForm/data/create-budget.ts
@@ -4,7 +4,7 @@ import axios from "axios"
 export const useCreateBudget = () => {
   const mutation = useMutation({
     mutationFn: (budget) => {
-      return axios.post('http://3.139.100.195:3000/budget', budget)
+      return axios.post('http://54.221.50.239:3000/budget', budget)
     },
   })
 
