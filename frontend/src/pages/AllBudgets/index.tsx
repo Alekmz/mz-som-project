5ebcaffcec8110383d582dea128d45794a280d3a
Josué Morais <andrejosueplay@gmail.com>
2024-09-10 19:40:19 -0300

    Merge branch 'feature-budgets' of https://github.com/Alekmz/front-mz-som into feature-budgets

--- frontend/src/pages/AllBudgets/index.tsx
+++ frontend/src/pages/AllBudgets/index.tsx
@@ -6,13 +6,13 @@ import { format } from "date-fns";
 import { useGetAllBudgets } from "./data/getAllBudgets";
 
 const AllBudgets = () => {
-const {data} = useGetAllBudgets();
-    return (
-        <div className="bg-white w-full h-screen flex flex-col">
+  const { data } = useGetAllBudgets();
+  return (
+    <div className="bg-white w-full h-screen flex flex-col">
       <Header />
       <Sidebar />
       <div className="p-4 md:ml-56 flex-grow">
-        <div className=" p-4 dark:border-gray-700 mt-20 md:mt-24 sm:mt-28">
+        <div className=" p-4 dark:border-gray-700 mt-20 md:mt-24 sm:mt-28 mb-6">
           {/* Cabeçalho */}
           <div className="mb-4">
             <div className="flex pb-3 justify-center dark:bg-gray-800">
@@ -114,15 +114,12 @@ const {data} = useGetAllBudgets();
                         className="text-gray-900 rounded-lg dark:text-white hover:text-[#2190BF] dark:hover:text-[#2190BF] group"
                       >
                         <svg
-                          className="w-5 hover:text-[#2190BF] dark:hover:text-[#2190BF]"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
+                          viewBox="0 0 1024 1024"
                           fill="currentColor"
-                          aria-hidden="true"
-                          stroke-width="1.5"
-                          stroke="currentColor"
+                          height="1.5em"
+                          width="1.5em"
                         >
-                          <path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z" />
+                          <path d="M531.3 574.4l.3-1.4c5.8-23.9 13.1-53.7 7.4-80.7-3.8-21.3-19.5-29.6-32.9-30.2-15.8-.7-29.9 8.3-33.4 21.4-6.6 24-.7 56.8 10.1 98.6-13.6 32.4-35.3 79.5-51.2 107.5-29.6 15.3-69.3 38.9-75.2 68.7-1.2 5.5.2 12.5 3.5 18.8 3.7 7 9.6 12.4 16.5 15 3 1.1 6.6 2 10.8 2 17.6 0 46.1-14.2 84.1-79.4 5.8-1.9 11.8-3.9 17.6-5.9 27.2-9.2 55.4-18.8 80.9-23.1 28.2 15.1 60.3 24.8 82.1 24.8 21.6 0 30.1-12.8 33.3-20.5 5.6-13.5 2.9-30.5-6.2-39.6-13.2-13-45.3-16.4-95.3-10.2-24.6-15-40.7-35.4-52.4-65.8zM421.6 726.3c-13.9 20.2-24.4 30.3-30.1 34.7 6.7-12.3 19.8-25.3 30.1-34.7zm87.6-235.5c5.2 8.9 4.5 35.8.5 49.4-4.9-19.9-5.6-48.1-2.7-51.4.8.1 1.5.7 2.2 2zm-1.6 120.5c10.7 18.5 24.2 34.4 39.1 46.2-21.6 4.9-41.3 13-58.9 20.2-4.2 1.7-8.3 3.4-12.3 5 13.3-24.1 24.4-51.4 32.1-71.4zm155.6 65.5c.1.2.2.5-.4.9h-.2l-.2.3c-.8.5-9 5.3-44.3-8.6 40.6-1.9 45 7.3 45.1 7.4zm191.4-388.2L639.4 73.4c-6-6-14.1-9.4-22.6-9.4H192c-17.7 0-32 14.3-32 32v832c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V311.3c0-8.5-3.4-16.7-9.4-22.7zM790.2 326H602V137.8L790.2 326zm1.8 562H232V136h302v216a42 42 0 0042 42h216v494z" />
                         </svg>
                       </Link>
                     </div>
@@ -134,7 +131,7 @@ const {data} = useGetAllBudgets();
       </div>
       <Footer />
     </div>
-    )
+  )
 };
 
 export default AllBudgets;
